14ad6c05c512de140e9916aec81cfb6f8e55b1ea
notyours <117538726+soulmeg@users.noreply.github.com>
2025-06-15 19:24:36 +0300

    Create ClientService.js

---
+++ metier/metierServices/ClientService.js
@@ -0,0 +1,16 @@
+const pool = require("../../config/db");
+const Client = require("../modele/Client");
+
+async function rechercherClientParPseudo(pseudo, motDePasse) {
+  const res = await pool.query(
+    "SELECT * FROM client WHERE pseudo = $1 AND motdepasse = $2",
+    [pseudo, motDePasse]
+  );
+
+  if (res.rows.length === 0) {
+    return null;
+  }
+  const c = res.rows[0];
+  return new Client(c.numero, c.pseudo, c.motdepasse, c.nom, c.prenom);
+}
+module.exports = { rechercherClientParPseudo };
