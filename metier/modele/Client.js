6ad26085db622de3ddfd7d2233653c3eda3bf1dd
notyours <117538726+soulmeg@users.noreply.github.com>
2025-06-15 19:17:48 +0300

    Create Client.js

---
+++ metier/modele/Client.js
@@ -0,0 +1,10 @@
+class Client {
+  constructor(numero, pseudo, motDePasse, nom, prenom) {
+    this.numero = numero;
+    this.pseudo = pseudo;
+    this.motDePasse = motDePasse;
+    this.nom = nom;
+    this.prenom = prenom;
+  }
+}
+module.exports = Client;
